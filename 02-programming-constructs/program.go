598a3e18bf42c5722203634997c5c626d4736f43
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-18 14:27:29 +0530

    if construct

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	/*
+		no := 21
+		if no%2 == 0 {
+			fmt.Printf("%d is even\n", no)
+		} else {
+			fmt.Printf("%d is odd\n", no)
+		}
+	*/
+
+	if no := 21; no%2 == 0 {
+		fmt.Printf("%d is even\n", no)
+	} else {
+		fmt.Printf("%d is odd\n", no)
+	}
+	fmt.Println(no)
 }
