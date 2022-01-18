7bd0ffc695b13caa71370034da8cd637cf44e4bb
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-18 16:46:43 +0530

    anonymous functions

---
+++ 03-functions/02-demo.go
@@ -0,0 +1,20 @@
+//anonymous functions
+
+package main
+
+import "fmt"
+
+func main() {
+	func() {
+		fmt.Println("fn invoked")
+	}()
+
+	func(x, y int) {
+		fmt.Println("add result =", x+y)
+	}(100, 200)
+
+	subResult := func(x, y int) int {
+		return x - y
+	}(100, 200)
+	fmt.Println("subtract result =", subResult)
+}
