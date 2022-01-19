f819df1f39be71da1c28253405e885762d7aed44
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-19 13:04:56 +0530

    Deleting from a slice

--- 04-collections/program.go
+++ 04-collections/program.go
@@ -36,29 +36,56 @@ func main() {
 	fmt.Println("Slice")
 	//var products []string = []string{"Pen", "Pencil", "Marker"}
 	//var products []string
-	var products []string = make([]string, 3, 10)
+	/* var products []string = make([]string, 3, 10)
 	fmt.Println(products, len(products), cap(products))
 	products[0] = "Pen"
 	products[1] = "Pencil"
 	products[2] = "Marker"
+	fmt.Println(products, len(products), cap(products)) */
+	var products []string
+	products = append(products, "Pencil", "Marker", "Duster")
 	fmt.Println(products, len(products), cap(products))
+	products = append(products, "Mouse")
+	fmt.Println(products, len(products), cap(products))
+	products = append(products, "Scribble-pad", "Stylus")
+	fmt.Println(products, len(products), cap(products))
+	products = append(products, "Printer")
+	fmt.Println(products, len(products), cap(products))
+
+	fmt.Println("Slicing")
+	fmt.Println("Deleting an item from a slice")
+	fmt.Println("Before deleting ", products)
+	products = append(products[0:3], products[4:]...)
+	fmt.Println("After deleting products[3]", products)
 	/*
-		products = append(products, "Pencil", "Marker", "Duster")
-		fmt.Println(products, len(products), cap(products))
-		products = append(products, "Mouse")
-		fmt.Println(products, len(products), cap(products))
-		products = append(products, "Scribble-pad", "Stylus")
-		fmt.Println(products, len(products), cap(products))
-		products = append(products, "Printer")
-		fmt.Println(products, len(products), cap(products)) */
+		[lo : hi] => from lo to hi-1
+		[lo:] => from lo to len-1
+		[:hi] => from 0 to hi-1
+		[:] => copy of the slice
+	*/
+	fmt.Println("products[2:5] =>", products[2:5])
+	fmt.Println("products[2:] =>", products[2:])
+	fmt.Println("products[:5] =>", products[:5])
+	//newProducts := products[:]
+	//newProducts[0] = "Charger"
+	newProducts := make([]string, len(products))
+	copy(newProducts, products)
+	newProducts = append(newProducts, "Charger")
+	fmt.Println(newProducts, products)
+	fmt.Println(len(newProducts), len(products))
+	products = append(products, "Adapter")
+	fmt.Println(newProducts, products)
+	fmt.Println(len(newProducts), len(products))
 
 	fmt.Println()
+
 	fmt.Println("Map")
 	//var productRanks map[string]int = make(map[string]int)
 	/*
 		productRanks := map[string]int{}
 		productRanks["Pen"] = 4
 	*/
+
 	productRanks := map[string]int{
 		"Pen":    4,
 		"Pencil": 1,
