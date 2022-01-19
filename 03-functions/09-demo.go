86554579e009ee740eb78ac5a52775c99074a766
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-19 10:18:12 +0530

    creating & handling errors

---
+++ 03-functions/09-demo.go
@@ -0,0 +1,32 @@
+/* error handling */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var DivideByZeroError = errors.New("divisor cannot be zero")
+
+func main() {
+	result, err := divide(100, 0)
+	if err == DivideByZeroError {
+		fmt.Println("Dont try to divide by zero")
+		return
+	}
+	if err != nil {
+		fmt.Println("Something went wront!")
+		fmt.Println(err)
+		return
+	}
+	//result, _ := divide(100, 0)
+	fmt.Println("result = ", result)
+}
+
+func divide(x, y int) (int, error) {
+	if y == 0 {
+		return 0, DivideByZeroError
+	}
+	return x / y, nil
+}
