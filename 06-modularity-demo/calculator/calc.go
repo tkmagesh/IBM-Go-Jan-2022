49286a7068dc10427f5f8e04017d45b001e30f95
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-19 17:26:03 +0530

    Day-02 complete

--- 06-modularity-demo/calculator/calc.go
+++ 06-modularity-demo/calculator/calc.go
@@ -1,6 +1,12 @@
 package calculator
 
+import "fmt"
+
 var opCount int
+
+func init() {
+	fmt.Println("calculator.init invoked")
+}
 
 func GetOpCount() int {
 	return opCount
--- 06-modularity-demo/main.go
+++ 06-modularity-demo/main.go
@@ -17,13 +17,29 @@ func main() {
 
 import (
 	"fmt"
+
+	"github.com/fatih/color"
 	calc "modularity-demo/calculator"
 	"modularity-demo/calculator/utils"
+	_ "modularity-demo/dummy"
 )
 
 func main() {
 	fmt.Println(calc.Add(100, 200))
 	fmt.Println(calc.Subtract(100, 200))
 	fmt.Printf("Op Count = %d\n", calc.GetOpCount())
-	fmt.Println(utils.IsPrime(97))
+	color.Blue("Is 97 a prime number ? : %t\n", utils.IsPrime(97))
 }
+
+/* import (
+	"fmt"
+	. "modularity-demo/calculator"
+	"modularity-demo/calculator/utils"
+)
+
+func main() {
+	fmt.Println(Add(100, 200))
+	fmt.Println(Subtract(100, 200))
+	fmt.Printf("Op Count = %d\n", GetOpCount())
+	fmt.Println(utils.IsPrime(97))
+} */
