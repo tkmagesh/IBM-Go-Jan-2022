38e5f6f52cf707bdce7cac2a7c592bfdf7250662
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-21 12:01:59 +0530

    resolving deadlocks involving wait groups & channels

--- 12-concurrency/demo-10.go
+++ 12-concurrency/demo-10.go
@@ -2,26 +2,21 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 //Share memory by communicating (using channels)
 func main() {
-	wg := &sync.WaitGroup{}
+
 	ch := make(chan int) //non-buffered channel
-	wg.Add(1)
-	go add(100, 200, wg, ch)
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
 	fmt.Println(result)
 
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	time.Sleep(2 * time.Second)
 	result := x + y
 	ch <- result
-	wg.Done()
-
 }
