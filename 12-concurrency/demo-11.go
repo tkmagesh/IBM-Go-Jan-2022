e6eacad1511ae06c3af6d732422ed0aa4d0f8b3d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-21 12:40:32 +0530

    testing buffered channels

--- 12-concurrency/demo-11.go
+++ 12-concurrency/demo-11.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,14 +13,23 @@ import (
 */
 
 func main() {
-	print("Hello")
-	print("World")
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	go print("Hello", ch1, ch2, wg)
+	go print("World", ch2, ch1, wg)
+	ch1 <- true
+	wg.Wait()
 	fmt.Println("End of main!")
 }
 
-func print(s string) {
+func print(s string, ch1, ch2 chan bool, wg *sync.WaitGroup) {
 	for i := 0; i < 5; i++ {
+		<-ch1
 		fmt.Println(s)
 		time.Sleep(500 * time.Millisecond)
+		ch2 <- true
 	}
+	wg.Done()
 }
